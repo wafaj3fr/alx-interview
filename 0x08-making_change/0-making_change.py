2c3dffc6efd5e71b8211fe11741461a222ecbca3
wafa <wafaagaafar@gmail.com>
2024-09-19 23:39:04 +0000

    0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,25 +5,21 @@ Function to determine the fewest number of coins needed to make the given total.
 
 def makeChange(coins, total):
     """
-    Args:
-    coins (list): A list of integers representing coin denominations.
-    total (int): The total amount to achieve using the fewest coins.
-    
-    Returns:
-    int: The fewest number of coins needed to make the total. If it's not possible, return -1.
+    Generate changes needed to reach total
+
     """
     if total <= 0:
         return 0
-    
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0 
-    
-
-    for coin in coins:
-    
-        for x in range(coin, total + 1):
-            dp[x] = min(dp[x], dp[x - coin] + 1)
-    
-    return dp[total] if dp[total] != float('inf') else -1
+    test = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while test < total:
+            test += i
+            temp += 1
+        if test == total:
+            return temp
+        test -= i
+        temp -= 1
+    return -1
 
